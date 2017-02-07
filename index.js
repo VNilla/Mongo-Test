ce29a6886935f8d67061da19217f4c52378aaa8a
Jamie Fox-Canning <jgf8395@g.rit.edu>
2017-02-06 17:21:50 -0800

    Install MongoDB and Mongoose, implement MongoDB
    Also fixed spelling of page title

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+var url = process.env.MONGOLAB_URI;
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -10,6 +15,19 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log("Unable to connect to the mongoDB server. Error:", err);
+    }
+    else {
+      console.log("Connection established to ", url);
+
+      //do stuff
+
+      db.close();
+    }
+  });
 });
 
 app.listen(app.get('port'), function(){
